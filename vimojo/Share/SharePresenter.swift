9e69e6007ccfd921daa57de71d50737908b911c9
Alejandro Arjonilla Garcia <aarjonilla@videona.com>
2016-12-20 09:46:32 +0100

    Hide social networks, share by generic share enabled and FTP

--- vimojo/Share/SharePresenter.swift
+++ vimojo/Share/SharePresenter.swift
@@ -94,7 +94,15 @@ class SharePresenter:NSObject,SharePresenterInterface{
     
     func pushGenericShare() {
         if let url = videoURL{
-            delegate?.showShareGeneric(url)
+            
+            GetPHAssetFromUrl().PHAssetForFileURL(url:url as NSURL, completion: {
+                phasset in
+                
+                ExportTemporalVideoToShare().exportVideoAsset(phasset, completion: {
+                    temporalVideo in
+                    self.delegate?.showShareGeneric(temporalVideo)
+                })
+            })
         }
     }
     
--- vimojo/Share/SocialNetworkProvider.swift
+++ vimojo/Share/SocialNetworkProvider.swift
@@ -23,30 +23,30 @@ class SocialNetworkProvider:NSObject{
             title: "Breaking News",
             action: ShareFTPBreakingNewsInteractor(delegate: delegate)))
         
-        socialNetworks.append( SocialNetwork(iconId: "activity_edit_share_facebook_normal",
-            iconIdPressed: "activity_edit_share_facebook_pressed",
-            title: "Facebook",
-            action: ShareFacebookInteractor(delegate: delegate)))
-        
-        socialNetworks.append( SocialNetwork(iconId: "activity_edit_share_instagram_normal",
-            iconIdPressed: "activity_edit_share_instagram_pressed",
-            title: "Instagram",
-            action: ShareInstagramInteractor(delegate: delegate)))
-        
-        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_twitter_normal",
-            iconIdPressed: "activity_edit_share_twitter_pressed",
-            title: "Twitter",
-            action: ShareTwitterInteractor(delegate: delegate)))
-        
-        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_whatsapp_normal",
-            iconIdPressed: "activity_edit_share_whatsapp_pressed",
-            title: "Whatsapp",
-            action: ShareWhatsappInteractor(delegate: delegate)))
-        
-        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_youtube_normal",
-            iconIdPressed: "activity_edit_share_youtube_pressed",
-            title: "Youtube",
-            action: ShareYoutubeInteractor(delegate: delegate)))
+//        socialNetworks.append( SocialNetwork(iconId: "activity_edit_share_facebook_normal",
+//            iconIdPressed: "activity_edit_share_facebook_pressed",
+//            title: "Facebook",
+//            action: ShareFacebookInteractor(delegate: delegate)))
+//        
+//        socialNetworks.append( SocialNetwork(iconId: "activity_edit_share_instagram_normal",
+//            iconIdPressed: "activity_edit_share_instagram_pressed",
+//            title: "Instagram",
+//            action: ShareInstagramInteractor(delegate: delegate)))
+//        
+//        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_twitter_normal",
+//            iconIdPressed: "activity_edit_share_twitter_pressed",
+//            title: "Twitter",
+//            action: ShareTwitterInteractor(delegate: delegate)))
+//        
+//        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_whatsapp_normal",
+//            iconIdPressed: "activity_edit_share_whatsapp_pressed",
+//            title: "Whatsapp",
+//            action: ShareWhatsappInteractor(delegate: delegate)))
+//        
+//        socialNetworks.append(SocialNetwork(iconId: "activity_edit_share_youtube_normal",
+//            iconIdPressed: "activity_edit_share_youtube_pressed",
+//            title: "Youtube",
+//            action: ShareYoutubeInteractor(delegate: delegate)))
         
         return socialNetworks
     }
