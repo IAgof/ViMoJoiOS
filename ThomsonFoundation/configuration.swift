41c2ad7d35282997ea1d09e14875d92362ed3a0b
DevStarlight <jesushuertaarrabal@gmail.com>
2017-08-09 12:59:12 +0200

    Adding assets, Info.plist & config file

---
+++ ThomsonFoundation/configuration.swift
@@ -0,0 +1,31 @@
+//
+//  configuration.swift
+//  vimojo
+//
+//  Created by Jesus Huerta on 08/08/2017.
+//  Copyright © 2017 Videona. All rights reserved.
+//
+
+import Foundation
+
+var configuration:ConfigurationProtocol = ThomsonFoundationConfiguration()
+
+final class ThomsonFoundationConfiguration: ConfigurationProtocol {
+	internal var fontName: String
+	internal var mainColor: UIColor
+	internal var plainButtonColor: UIColor
+	internal var secondColor: UIColor
+	internal var secondColorWithOpacity: UIColor
+	internal var VOICE_OVER_FEATURE: Bool
+	internal var FTP_FEATURE: Bool
+	
+	init() {
+		fontName = "Helvetica"
+		mainColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+		plainButtonColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+		secondColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+		secondColorWithOpacity = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.5)
+		VOICE_OVER_FEATURE = true
+		FTP_FEATURE = true
+	}
+}
