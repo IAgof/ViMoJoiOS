0c903f4a6201fd09f6363d69e2e4a4d7fdcfb0d1
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2017-11-09 19:42:56 +0100

    Problem with Dsyms file https://stackoverflow.com/questions/40581941/crashlytics-not-working-ios-10-xcode-8-1

---
+++ vimojo/Recorder/CameraView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraView.swift
+//  vimojo
+//
+//  Created by Jesús Huerta Arrabal on 9/11/17.
+//  Copyright © 2017 Videona. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
