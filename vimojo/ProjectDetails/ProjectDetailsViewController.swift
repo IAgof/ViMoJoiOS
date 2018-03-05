5110e09ebbbb4fb367ca0d1ae70df937a1ecfa68
Alejandro Arjonilla Garcia <alejandro@mbpdealejandro3.home>
2018-03-05 22:36:29 +0100

    Remove navigation bar and removed wrong placeholders

--- vimojo/ProjectDetails/ProjectDetailsViewController.swift
+++ vimojo/ProjectDetails/ProjectDetailsViewController.swift
@@ -38,6 +38,14 @@ class ProjectDetailsViewController: UIViewController, ProjectDetailsViewProtocol
         addObserverToShowAndHideKeyboard()
         descriptionTextView.delegate = self
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
     }
