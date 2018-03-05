3572c9ff30aac73b8e194bf6b348a1e7a2809cf5
Alejandro Arjonilla Garcia <alejandro@mbpdealejandro3.home>
2018-03-05 21:53:11 +0100

    Update RealmMigration scheme number to update automatically DB

--- vimojo/Repository/RealmMigrationsUseCase.swift
+++ vimojo/Repository/RealmMigrationsUseCase.swift
@@ -10,21 +10,15 @@ import Foundation
 import RealmSwift
 
 class RealmMigrationsUseCase {
-    let newSchemaVersion = UInt64(5)
-    let oldSchemaVersion = UInt64(4)
+    let newSchemaVersion = UInt64(6)
+    let oldSchemaVersion = UInt64(5)
 
     func updateMigrationDefault() {
         // Inside your application(application:didFinishLaunchingWithOptions:)
 
         let config = Realm.Configuration(
-            // Set the new schema version. This must be greater than the previously used
-            // version (if you've never set a schema version before, the version is 0).
             schemaVersion: newSchemaVersion,
-
-            // Set the block which will be called automatically when opening a Realm with
-            // a schema version lower than the one set above
             migrationBlock: { _, oldSchemaVersion in
-                // We haven’t migrated anything yet, so oldSchemaVersion == 0
                 if (oldSchemaVersion < self.oldSchemaVersion) {
                     // Nothing to do!
                     // Realm will automatically detect new properties and removed properties
@@ -34,9 +28,6 @@ class RealmMigrationsUseCase {
 
         // Tell Realm to use this new configuration object for the default Realm
         Realm.Configuration.defaultConfiguration = config
-
-        // Now that we've told Realm how to handle the schema change, opening the file
-        // will automatically perform the migration
         do {
 			try Realm()
         } catch {
