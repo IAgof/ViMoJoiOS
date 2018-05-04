89f699fb952c18c5658266c7069c1a7e762306a9
DevStarlight <jesushuertaarrabal@gmail.com>
2018-05-04 13:22:36 +0200

    added TODO for mooving the condition to the mapper

--- vimojo/Repository/ProjectRealmRepository.swift
+++ vimojo/Repository/ProjectRealmRepository.swift
@@ -79,6 +79,7 @@ public class ProjectRealmRepository: ProjectRepository {
                 if let result = realm.objects(RealmProject.self).sorted(byProperty: "modificationDate", ascending: false).first {
                     project = self.toProjectMapper.map(from: result)
                 } else {
+                    // TODO (DevStarlight): Let's see how can we move this to the mapper
                     if !configuration.IS_WATERMARK_SWITCHABLE && !configuration.IS_WATERMARK_PURCHABLE {
                         project.hasWatermark = configuration.IS_WATERMARK_ENABLED
                     }
